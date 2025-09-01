668e9653f94caa180bc701eacba53ef4b3e3fb5a
kpmax03 <kp5911800@gmail.com>
2025-09-01 13:25:33 +0530

    added domain and techs enum in userProfile for searching

---
+++ src/main/java/com/dev/connect/exception/WantsToGiveException.java
@@ -0,0 +1,8 @@
+package com.dev.connect.exception;
+
+public class WantsToGiveException extends RuntimeException {
+
+    public WantsToGiveException(String message) {
+        super(message);
+    }
+}
